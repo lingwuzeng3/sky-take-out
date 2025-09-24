0f8d8fba9f7961706801481b9e02af30b47f90c8
lingwuzeng3 <z09103839@163.com>
2025-09-24 22:08:26 +0800

    添加员工分页查询，处理了时间格式

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -2,11 +2,14 @@ package com.sky.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.sky.dto.EmployeeDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface EmployeeMapper{
@@ -24,4 +27,6 @@ public interface EmployeeMapper{
             "(#{name},#{username},#{password},#{phone},#{sex},#{idNumber},#{createTime},#{updateTime},#{createUser},#{updateUser})"
     )
     void save(Employee employee);
+
+    List<Employee> page(EmployeePageQueryDTO employeePageQueryDTO);
 }
